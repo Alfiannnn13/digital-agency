c364325aa2be7794a612025795dce47a0ebbe1e4
Alfian <muhammadalfiannovanto17@gmail.com>
2025-05-19 17:47:20 +0700

    styling hero section

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// add event on multiple el
+
+const addEventOnElement = function (elements, evetType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(evetType, callback);
+  }
+};
+
+// navbar toggle func
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggleBtn.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElement([navToggleBtn, overlay], "click", toggleNavbar);
